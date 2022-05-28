f0659337c71e7e548bac36521ad5126e5a13058b
WijsenWesley <wesley.wijsen@ida-mediafoundry.be>
2022-05-28 15:56:06 +0200

    Played with generics

--- src/generics/Pair.java
+++ src/generics/Pair.java
@@ -1,13 +1,31 @@
 package generics;
 
 public class Pair<A, B> {
-    public A a;
-    public B b;
+    // Heeft het nut om die generic fields hier private te zetten??
+    // Is public logischer, of hangt het af van de use case??
+    private A a;
+    private B b;
 
     protected Pair (){}
 
     public Pair (A a, B b){
         this.a = a;
+        this.b = b;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public void setA(A a) {
+        this.a = a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    public void setB(B b) {
         this.b = b;
     }
 }
--- src/generics/PairOfPairs.java
+++ src/generics/PairOfPairs.java
@@ -4,15 +4,15 @@ public class PairOfPairs {
     public static void main(String[] args) {
         Pair<String, Integer> score1 = new Pair<>("Will", 60);
         System.out.println("-------Pair of String and Integer-------");
-        System.out.println(score1.a + " : " + score1.b );
+        System.out.println(score1.getA() + " : " + score1.getB());
 
 //        score1.a = 60;
 //        score1.b = "Will";
 
         Pair<String, String> stringsPair = new Pair<>("This", "That");
         System.out.println("-------Pair of String and String-------");
-        System.out.println(stringsPair.a + " : " + stringsPair.b );
-        stringsPair.b = "Will";
-        System.out.println(stringsPair.a + " : " + stringsPair.b );
+        System.out.println(stringsPair.getA() + " : " + stringsPair.getB());
+        stringsPair.setB("Will");
+        System.out.println(stringsPair.getA() + " : " + stringsPair.getB());
     }
 }
